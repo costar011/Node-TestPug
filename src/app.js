692f569072dbd1803647e7a45db95d89e4d093c8
YERIM <dnaer456@gmail.com>
2020-06-19 09:51:26 +0900

    CSS Fail

--- src/app.js
+++ src/app.js
@@ -5,7 +5,13 @@ const PORT = 3000;
 const app = express();
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "/assets")));
-
+// 있어야할 곳? => /views
+app.get("/", (req, res) => {
+  res.render("test");
+});
+app.get("/on", (req, res) => {
+  res.send("<h1>Hello node</h1>");
+});
 app.listen(PORT, () => {
   console.log(`${PORT} server start`);
 });
